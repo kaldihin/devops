6a4c1b7d46a61f2bfbc79f94f7d9698a5dad5214
Eugenii Kaldikhin <ekaldikhin@griddynamics.com>
2022-05-11 10:35:21 +0000

    Corrected configuration

--- Terraform/ilb/main.tf
+++ Terraform/ilb/main.tf
@@ -5,15 +5,20 @@ terraform {
 }
 
 provider "google" {
-  project     = var.project_name
-#  credentials = file("kopicloud-tfadmin.json")
-  region      = var.gcp_region_1
-  zone        = var.gcp_zone_1
+  project = var.project_name
+  #  credentials = file("kopicloud-tfadmin.json")
+  region = var.gcp_region_1
+  zone   = var.gcp_zone_1
 }
 
 provider "google-beta" {
-  project     = var.project_name
-#   credentials = file(var.gcp_auth_file)
-  region      = var.gcp_region_1
-  zone        = var.gcp_zone_1
+  project = var.project_name
+  #   credentials = file(var.gcp_auth_file)
+  region = var.gcp_region_1
+  zone   = var.gcp_zone_1
+}
+
+resource "google_service_account" "service_account" {
+  account_id   = "compute78695"
+  display_name = "Service Account"
 }
--- Terraform/ilb/variables.tf
+++ Terraform/ilb/variables.tf
@@ -14,7 +14,7 @@ variable "gce_ssh_pub_key_file" {
 }
 
 variable "project_name" {
-  type = string
+  type    = string
   default = "boxwood-yen-347112"
 }
 
@@ -25,28 +25,35 @@ variable "project_name" {
 # }
 
 # define GCP project name
-variable "app_project" {
-  type = string
+variable "app_name" {
+  type        = string
   description = "GCP project name"
-  default = "boxwood-yen-347112"
+  default     = "boxwood-yen-347112"
+}
+
+# define GCP project name
+variable "app_project" {
+  type        = string
+  description = "GCP project name"
+  default     = "boxwood-yen-347112"
 }
 
 # define GCP region
 variable "gcp_region_1" {
-  type = string
+  type        = string
   description = "GCP region"
-  default = "us-central1"
+  default     = "us-central1"
 }
 
 # define GCP zone
 variable "gcp_zone_1" {
-  type = string
+  type        = string
   description = "GCP zone"
-  default = "us-central1-a"
+  default     = "us-central1-a"
 }
 
 # define private subnet
 variable "private_subnet_cidr_1" {
-  type = string
+  type        = string
   description = "private subnet CIDR 1"
 }
